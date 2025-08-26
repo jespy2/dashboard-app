9bf5d113306a6552f2a11b51eb613939e4ad2b00
jespy2 <jespy2@gmail.com>
2025-08-26 14:14:32 -0600

    import line breaking linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,10 +10,25 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    plugins: {
+      import: require('eslint-plugin-import'),
+    },
+  },
   {
     rules: {
-      quotes: ["error", "single", { avoidEscape: true }],
+      quotes: ['error', 'single', { avoidEscape: true }],
+      // Key rule for wrapping import members:
+      'object-curly-newline': [
+        'error',
+        {
+          ImportDeclaration: {
+            multiline: true,
+            minProperties: 2, // force line breaks if >= 2 members
+          },
+        },
+      ],
     },
   },
 ];
